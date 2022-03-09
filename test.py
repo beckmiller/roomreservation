62658aa2225a660e745ea8483236b14e5242f29f
beckmiller <erifieder@gmail.com>
2022-03-09 15:07:31 +0300

    Change some documentation and some variables.

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from db import create_connection
 from dataclasses import dataclass
 
@@ -17,10 +16,13 @@ class Person:
 def get_room_avaibility(room_id, start_time, end_time):
     """Get information about room avaibility to reserve.
 
-    Keyword arguments:
-    room_id -- number of reserving room int(1)
-    start_time -- starting time reserve  str(10:00)
-    end_time --  ending time reserve str(18:00)
+    Args:
+        room_id (int): Number of reserving room
+        start_time (str): Start reservation time
+        end_time (str): End of reservation time
+
+    Returns:
+        int, str: It returns avaibility o froom
     """
     request_ = request_to_databse(room_id, start_time, end_time)
 
@@ -42,13 +44,23 @@ def __check_giving_argumetns(room_id, start_time, end_time):
 
 
 def request_to_databse(room_id, start_time, end_time):
+    """Requesting to database. Is there any available room.
+
+    Args:
+        room_id (int): Number of reserving room
+        start_time (str): Start reservation time
+        end_time (str): End of reservation time
+
+    Returns:
+        sqlite_cursor: Returns respons from database
+    """
     cursor = conn.cursor()
     if __check_giving_argumetns(room_id, start_time, end_time):
-        room_id_query = cursor.execute("SELECT number FROM office \
+        query_response = cursor.execute("SELECT number FROM office \
                                         WHERE number = ? and \
                                         (start_time = ? or end_time = ?);",
                                         (room_id, start_time, end_time, ))
-        return room_id_query
+        return query_response
     else:
         return 'Please be sure that you \
                 \ngiving room_id as int and \
@@ -58,32 +70,36 @@ def request_to_databse(room_id, start_time, end_time):
 def get_reserved_person(room_id, start_time, end_time):
     """Returns person by whom reserved"""
     cursor = conn.cursor()
-    if_busy_query = cursor.execute("SELECT name, start_time, end_time \
+    query_response = cursor.execute("SELECT name, start_time, end_time \
                                     FROM person INNER JOIN office \
                                     ON person.id = office.person_id \
                                     WHERE office.number = ? and \
                                     (office.start_time = ? or \
                                     office.end_time =?)",
                                     (room_id, start_time, end_time,))
-    if_busy_response = if_busy_query.fetchall()
-    if if_busy_response:
-        for busy in if_busy_response:
-            response = f"Sorry, you can't reserve office {room_id}. \
-                    \nBecause office reserved by {busy[0]} \
-                    \nfrom {busy[1]} to {busy[2]}"
-        return response
+    all_responses = query_response.fetchone()
+    if all_responses:
+        name, resp_start_time, resp_end_time, = all_responses
+
+        return f"Sorry, you can't reserve office {room_id}. \
+                \nBecause office reserved by {name} \
+                \nfrom {resp_start_time} to {resp_end_time}"
+
     conn.commit()
     conn.close()
 
 
 def reserve(person, room_id, start_time, end_time):
-    """Reserve a room by giving arguments
+    """Reserving a room by giving arguments
 
-    Keyword arguments:
-    person -- instance of a class Person
-    room_id -- number of reserving room int(1)
-    start_time -- starting time reserve  str(10:00)
-    end_time --  ending time reserve strt(18:00)
+    Args:
+        person (class): Instance of a class Person
+        room_id (int): Number of reserving room
+        start_time (str): Start reservation time
+        end_time (str): End of reservation time
+
+    Returns:
+        int, str: Informing about reservation
     """
     cursor = conn.cursor()
     room_id_query = request_to_databse(room_id, start_time, end_time)
@@ -104,6 +120,10 @@ def reserve(person, room_id, start_time, end_time):
                         (None, room_id, person.name,
                             start_time, end_time, today,))
 
+        return f"{person.name} you successfully reserved \
+                \noffice №{room_id} from {start_time} \
+                \nto {end_time}."
+
     conn.commit()
     conn.close()
 
@@ -121,6 +141,7 @@ def reserve_is_none_update(person, room_id, start_time, end_time):
                     start_time = ?, end_time = ? date = ?',
                     (room_id, person.name,
                         start_time, end_time, today,))
+
     conn.commit()
     conn.close()
 
@@ -138,8 +159,9 @@ def send_email(person):
     date_, start_time, end_time, room_number, email, phone = query.fetchone()
 
     print(f"To {email} and {phone}: \
-        \nYou reserved office {room_number} from {start_time} to {end_time} \
-        \nat {date_}")
+            \nYou reserved office {room_number} \
+            \nfrom {start_time} to {end_time} \
+            \nat {date_}")
 
     conn.commit()
     conn.close()
@@ -152,3 +174,4 @@ if __name__ == '__main__':
     print(get_room_avaibility(4, '10:00', '11:00'))
     reserve(person3, 4, '10:00', '11:00')
     send_email(person1)
+
